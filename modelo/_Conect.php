ea85a520e72d7aee87fc5f28ddf40239b04f2cb9
MartinezLuis09 <107577645+MartinezLuis09@users.noreply.github.com>
2022-08-22 11:40:02 -0500

    A

--- modelo/_Conect.php
+++ modelo/_Conect.php
@@ -9,11 +9,15 @@
 					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 				);
 
-      		$BD1	= "rfwsmqex_ecommerce_fz";
+				$BD1	= "rfwsmqex_gvsl";
+      		$BD2	= "rfwsmqex_gvsl_sys_almacen";
 
 			try {
 				$this->mysql1 = new PDO('mysql:host='.$host.';dbname='.$BD1,$user,$pass,$opc);
 				$this->mysql1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				$this->mysql2 = new PDO('mysql:host='.$host.';dbname='.$BD2,$user,$pass,$opc);
+				$this->mysql2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			} catch (PDOException $e) {
 				echo "ERROR! ". $e->getMessage();
