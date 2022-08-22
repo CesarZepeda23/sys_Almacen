e9fac6f9c1f388b72089891824f540f5f7544114
CesarZepeda23 <88913951+CesarZepeda23@users.noreply.github.com>
2022-08-22 11:39:35 -0500

    Cambios

--- index.php
+++ index.php
@@ -6,16 +6,52 @@
 <body>
    <main>
        <section class="container">
-       <div class="container-fluid pt-4 px-4">
+            <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
-                <div class="col-12">
+                    <div class="col-sm-12 col-xl-6">
                         <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4">Responsive Table</h6>
+                            <h6 class="mb-4">Unidades De Negocio</h6>
                             <div class="table-responsive">
-                                <table class="table">
+                            <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th scope="col">#</th>
+                                            <th scope="col">idUDN</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Abreviatura</th>
+                                            <th scope="col">ColorUDN</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>John</td>
+                                            <td>Doe</td>
+                                            <td>jhon@email.com</td>
+                                            <td>USA</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>mark@email.com</td>
+                                            <td>UK</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Estado</h6>
+                            <div class="table-responsive">
+                            <table class="table table-striped">
+                                    <thead>
+                                        <tr>
                                             <th scope="col">First Name</th>
                                             <th scope="col">Last Name</th>
                                             <th scope="col">Email</th>
@@ -26,7 +62,6 @@
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <th scope="row">1</th>
                                             <td>John</td>
                                             <td>Doe</td>
                                             <td>jhon@email.com</td>
@@ -35,7 +70,6 @@
                                             <td>Member</td>
                                         </tr>
                                         <tr>
-                                            <th scope="row">2</th>
                                             <td>Mark</td>
                                             <td>Otto</td>
                                             <td>mark@email.com</td>
@@ -44,7 +78,6 @@
                                             <td>Member</td>
                                         </tr>
                                         <tr>
-                                            <th scope="row">3</th>
                                             <td>Jacob</td>
                                             <td>Thornton</td>
                                             <td>jacob@email.com</td>
@@ -57,8 +90,51 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                    <div class="col-sm-12">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Componentes</h6>
+                            <div class="table-responsive">
+                            <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Country</th>
+                                            <th scope="col">ZIP</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>John</td>
+                                            <td>Doe</td>
+                                            <td>jhon@email.com</td>
+                                            <td>USA</td>
+                                            <td>123</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>mark@email.com</td>
+                                            <td>UK</td>
+                                            <td>456</td>
+                                            <td>Member</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>jacob@email.com</td>
+                                            <td>AU</td>
+                                            <td>789</td>
+                                            <td>Member</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
             <!-- Table End -->
        </section>
    </main>
