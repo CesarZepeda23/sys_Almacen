bcca34fc82a2b893f53a70044a147571a3a9d735
CesarZepeda23 <88913951+CesarZepeda23@users.noreply.github.com>
2022-08-23 15:08:23 -0500

    E

--- controlador/ctrl_Equipos.php
+++ controlador/ctrl_Equipos.php
@@ -11,8 +11,8 @@ switch($opc){
         foreach($sql as $row){
             $tablaEquipos = '
                              <tr>
-                             <td>' . $row['UDN'] . ' </td>
-                             <td>' . $row['nombre'] . ' </td>
+                                <td>' . $row['UDN'] . ' </td>
+                                <td>' . $row['nombre'] . ' </td>
                                 <td>' . $row['numeroEquipo'] . ' </td>
                                 <td>' . $row['responsableEquipo'] . ' </td>
                                 <td>' . $row['fechaAlta'] . ' </td>
--- modelo/mdl_Equipos.php
+++ modelo/mdl_Equipos.php
@@ -2,19 +2,21 @@
 include_once("_CRUD.php");
 
 Class Equipos extends CRUD {
+
     function mostrarEquipos () {
         $query = "SELECT
         rfwsmqex_gvsl.udn.UDN,
-        rfwsmqex_gvsl_sys_almacen.areas.nombre,
         rfwsmqex_gvsl_sys_almacen.equipo.fechaAlta,
-        rfwsmqex_gvsl_sys_almacen.equipo.responsableEquipo,
-        rfwsmqex_gvsl_sys_almacen.equipo.numeroEquipo
+        rfwsmqex_gvsl_sys_almacen.areas.nombre,
+        rfwsmqex_gvsl_sys_almacen.equipo.numeroEquipo,
+        rfwsmqex_gvsl_sys_almacen.equipo.responsableEquipo
         FROM
         rfwsmqex_gvsl.udn ,
-        rfwsmqex_gvsl_sys_almacen.areas ,
-        rfwsmqex_gvsl_sys_almacen.equipo";
+        rfwsmqex_gvsl_sys_almacen.equipo ,
+        rfwsmqex_gvsl_sys_almacen.areas";
         $sql = $this->_Select($query,null,"2");
         return $sql;
     }
 }
+
 ?>
