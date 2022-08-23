1df55f33a511eae19d4ea5ab6984737f30eb7d6d
TaniaGisell <88916403+TaniaGisell@users.noreply.github.com>
2022-08-23 11:51:38 -0500

    a

--- vistas/areas.php
+++ vistas/areas.php
@@ -8,25 +8,62 @@ require("../vistas/navbar.php"); ?>
         <section class="container">
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
-                    <div class="col-sm-12">
+                <div class="col-sm-12">
                         <div class="bg-light rounded h-100 p-4">
                             <h6 class="mb-4">Componentes</h6>
                             <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th scope="col">ID</th>
+                                            <th scope="col">ID Del Componente</th>
                                             <th scope="col">Nombre</th>
                                             <th scope="col">Marca</th>
                                             <th scope="col">Modelo</th>
-                                            <th scope="col">UDS. Negocios</th>
-                                            <th scope="col">Area</th>>
                                             <th scope="col">Estado</th>
                                             <th scope="col">Costo</th>
-                                            <th scope="col">Acciones</th>
                                         </tr>
                                     </thead>
-                                    <tbody id="tablasUDN">
+                                    <tbody id="tablascomponentesindex">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Unidades De Negocio</h6>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Id De UDN</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Abreviatura</th>
+                                            <th scope="col">ColorUDN</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tablasUDNindex">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Equipo</h6>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Id Del Equipo</th>
+                                            <th scope="col">Fecha De Alta</th>
+                                            <th scope="col">Encargado Del Equipo</th>
+                                            <th scope="col">Id Del Componente</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tablasEquiposindex">
 
                                     </tbody>
                                 </table>
@@ -34,7 +71,7 @@ require("../vistas/navbar.php"); ?>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>        
         </section>
     </main>
 
