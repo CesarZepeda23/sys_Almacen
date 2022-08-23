65236c32a9c59a97b0d405ef9af4fb92746901ba
TaniaGisell <88916403+TaniaGisell@users.noreply.github.com>
2022-08-23 10:02:24 -0500

    AVANCE

---
+++ vistas/udn.php
@@ -0,0 +1,49 @@
+<!--ARCHIVOS PHP -->
+<?php include("../vistas/header.php");
+require("../vistas/navbar.php"); ?>
+<!-- INDEX -->
+
+<body>
+    <main>
+        <section class="container">
+            <div class="container-fluid pt-4 px-4">
+            <button type="button" class="btn btn-primary m-2"><i class="fa fa-pen me-2"></i>NuevA</button>
+                <div class="row g-4">
+                    <div class="col-sm-12">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Componentes</h6>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Marca</th>
+                                            <th scope="col">Modelo</th>
+                                            <th scope="col">UDS. Negocios</th>
+                                            <th scope="col">Area</th>>
+                                            <th scope="col">Estado</th>
+                                            <th scope="col">Costo</th>
+                                            <th scope="col">Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tablasUDN">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <?php include("../vistas/footer.php"); ?>
+
+    <!--ARCHIVOS PHP -->
+    <!--ARCHIVOS SCRIPTS -->
+    <script src="../recursos/js/udn.js"=<?php echo time(); ?>"></script>
+    <script src="../recursos/js/index.js"=<?php echo time(); ?>"></script>
+
+</body>
