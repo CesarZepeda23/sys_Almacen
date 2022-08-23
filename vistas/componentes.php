d877f8b0da0b1d6a5af1f77ba1a0bfa8c2645319
CesarZepeda23 <88913951+CesarZepeda23@users.noreply.github.com>
2022-08-23 09:45:30 -0500

    xd

--- vistas/componentes.php
+++ vistas/componentes.php
@@ -5,6 +5,7 @@
    <main>
        <section class="container">
             <div class="container-fluid pt-4 px-4">
+            <button type="button" class="btn btn-primary m-2"><i class="fa fa-pen me-2"></i>Nuevo Componente</button>
                 <div class="row g-4">
                     <div class="col-sm-12">
                         <div class="bg-light rounded h-100 p-4">
@@ -39,4 +40,6 @@
 <!--ARCHIVOS PHP -->
    <!--ARCHIVOS SCRIPTS -->
     <script src="../recursos/js/componentes.js"=<?php echo time(); ?>"></script>
+    <script src="../recursos/js/index.js"=<?php echo time(); ?>"></script>
+
 </body>
