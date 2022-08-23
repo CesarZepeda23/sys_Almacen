f60f96813a6c437c65b9aa3b053ee3c4d24ad46f
MartinezLuis09 <107577645+MartinezLuis09@users.noreply.github.com>
2022-08-23 16:27:12 -0500

    l

--- vistas/componentes.php
+++ vistas/componentes.php
@@ -2,18 +2,15 @@
 <?php include("../vistas/header.php");
 require("../vistas/navbar.php"); ?>
 <!-- INDEX -->
-<html>
 
-<body>
+<body style="min-height: 100vw;">
     <main>
         <section class="container">
             <div class="container-fluid pt-4 px-4">
-                <button type="button" class="btn btn-primary m-2" onclick="location.href='regComponentes.php'; target='_blank'">
-                    <i class="fa fa-pen me-2"></i>Nuevo Componente</button>
                 <div class="row g-4">
                     <div class="col-sm-12">
                         <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4">Componentes</h6>
+                            <h6 class="mb-4">UDN - Areas</h6>
                             <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
@@ -25,13 +22,11 @@ require("../vistas/navbar.php"); ?>
                                             <th scope="col">UDN. Negocios</th>
                                             <th scope="col">Area</th>
                                             <th scope="col">Condición</th>
-                                            <th scope="col">Area</th>
-                                            <th scope="col">Estado</th>
                                             <th scope="col">Costo</th>
                                             <th scope="col">Acciones</th>
                                         </tr>
                                     </thead>
-                                    <tbody id="tablascomponentes">
+                                    <tbody id="tablaComponentes">
 
                                     </tbody>
                                 </table>
@@ -46,11 +41,8 @@ require("../vistas/navbar.php"); ?>
     <?php include("../vistas/footer.php"); ?>
 
     <!--ARCHIVOS PHP -->
-
     <!--ARCHIVOS SCRIPTS -->
     <script src="../recursos/js/componentes.js"=<?php echo time(); ?>"></script>
     <script src="../recursos/js/index.js"=<?php echo time(); ?>"></script>
 
 </body>
-
-</html>
--- vistas/regComponentes.php
+++ vistas/regComponentes.php
@@ -1,23 +1,50 @@
 <!--ARCHIVOS PHP -->
 <?php include("../vistas/header.php");
 require("../vistas/navbar.php"); ?>
-<!-- INDEX -->
-<html>
 
-<body>
+<body style="min-height: 100vw;">
     <main>
-        <section class="container">
+        <section class="container-fluid p">
+            <!-- Form Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-12 ">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Registro de componentes</h6>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <label for="floatingSelect">Works with selects</label>
+                            </div>
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 150px;"></textarea>
+                                <label for="floatingTextarea">Comments</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Form End -->
         </section>
     </main>
 
     <?php include("../vistas/footer.php"); ?>
 
     <!--ARCHIVOS PHP -->
-
     <!--ARCHIVOS SCRIPTS -->
-    <script src="../recursos/js/componentes.js=<?php echo time(); ?>"></script>
-    <script src="../recursos/js/index.js=<?php echo time(); ?>"></script>
+    <script src="../recursos/js/componentes.js"=<?php echo time(); ?>"></script>
+    <script src="../recursos/js/index.js"=<?php echo time(); ?>"></script>
 
 </body>
-
-</html>
