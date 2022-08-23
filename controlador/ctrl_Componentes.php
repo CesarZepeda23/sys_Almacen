39ddc606dc148266bf27b4767017b54c76bf6ab2
MartinezLuis09 <107577645+MartinezLuis09@users.noreply.github.com>
2022-08-23 09:46:30 -0500

    j

--- controlador/ctrl_Componentes.php
+++ controlador/ctrl_Componentes.php
@@ -4,28 +4,31 @@ $obj = new Componentes;
 
 $opc    = $_POST['opc'];
 
-switch($opc){
-    case 1://MOSTRAR UDN
+switch ($opc) {
+    case 1: //MOSTRAR UDN
         $tablaComponentes = null;
-        $sql = $obj -> mostrarComponentes(); 
-        foreach($sql as $row){
-            $tablaComponentes = '
+        $sql = $obj->mostrarComponentes();
+        foreach ($sql as $row) {
+            $tablaComponentes =
+                '
                 <tr>
-                                <td>' . $row['idComponente'] . ' </td>
-                                <td>' . $row['nombre'] . ' </td>
-                                <td>' . $row['marca'] . ' </td>
-                                <td>' . $row['modelo'] . ' </td>
-                                <td>' . $row['estado'] . ' </td>
-                                <td>$ ' . $row['costo'] . ' </td>
-                                <td><button type="button" class="btn btn-square btn-primary m-1"><i class="fa fa-edit"></i></button>
-                                <button type="button" class="btn btn-square btn-warning m-1"><i class="fa fa-print"></i></button>
-                                <button type="button" class="btn btn-square btn-danger m-1"><i class="fa fa-trash"></i></button></td>
-                            </tr>
-            ';                                  
+                    <td>' . $row['idComponente'] . ' </td>
+                    <td>' . $row['nombre'] . ' </td>
+                    <td>' . $row['marca'] . ' </td>
+                    <td>' . $row['modelo'] . ' </td>
+                    <td>' . $row['uds'] . ' </td>
+                    <td>' . $row['area'] . ' </td>
+                    <td>' . $row['estado'] . ' </td>
+                    <td>$ ' . number_format($row['costo'], 2) . ' </td>
+                    <td>
+                        <button type="button" class="btn btn-square btn-primary m-1"><i class="fa fa-edit"></i></button>
+                        <button type="button" class="btn btn-square btn-warning m-1"><i class="fa fa-print"></i></button>
+                        <button type="button" class="btn btn-square btn-danger m-1"><i class="fa fa-trash"></i></button>
+                    </td>
+                </tr>
+            ';
 
             echo $tablaComponentes;
         }
         break;
-
-}   
-?>
+}
