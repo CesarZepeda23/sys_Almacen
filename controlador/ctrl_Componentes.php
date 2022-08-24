a4970019cda101a79d4a02129c38fb7772374421
MartinezLuis09 <107577645+MartinezLuis09@users.noreply.github.com>
2022-08-24 10:20:49 -0500

    Luis

--- controlador/ctrl_Componentes.php
+++ controlador/ctrl_Componentes.php
@@ -6,30 +6,38 @@ $opc    = $_POST['opc'];
 
 switch ($opc) {
     case 1:
-        $tablaComponentes = null;
-        $sql = $obj->mostrarComponentes();
+        $diseño = '<option selected value="0" disabled >Seleccione una Opción</option>';
+        $sql = $obj->mostrarUDN();
         foreach ($sql as $row) {
-            $tablaComponentes =
-                '
-                <tr>
-                    <td>' . $row['nombre'] . ' </td>
-                    <td>' . $row['nombre'] . ' </td>
-                    <td>' . $row['nombre'] . ' </td>
-                    <td>' . $row['modelo'] . ' </td>
-                    <td>' . $row['nombre'] . ' </td>
-                    <td>' . $row['udn'] . ' </td>
-                    <td>' . $row['area'] . ' </td>
-                    <td>' . $row['estado'] . ' </td>
-                    <td>' . $row['modelo'] . ' </td>
-                    <td>
-                        <button type="button" class="btn btn-square btn-primary m-1"><i class="fa fa-edit"></i></button>
-                        <button type="button" class="btn btn-square btn-warning m-1"><i class="fa fa-print"></i></button>
-                        <button type="button" class="btn btn-square btn-danger m-1"><i class="fa fa-trash"></i></button>
-                    </td>
-                </tr>
-            ';
-
-            echo $tablaComponentes;
+            $diseño .= '<option value="' . $row['idUDN'] . '">' . $row['UDN'] . '</option>';
         }
+        echo $diseño;
         break;
+        // case 1:
+        //     $tablaComponentes = null;
+        //     $sql = $obj->mostrarComponentes();
+        //     foreach ($sql as $row) {
+        //         $tablaComponentes =
+        //             '
+        //             <tr>
+        //                 <td>' . $row['nombre'] . ' </td>
+        //                 <td>' . $row['nombre'] . ' </td>
+        //                 <td>' . $row['nombre'] . ' </td>
+        //                 <td>' . $row['modelo'] . ' </td>
+        //                 <td>' . $row['nombre'] . ' </td>
+        //                 <td>' . $row['udn'] . ' </td>
+        //                 <td>' . $row['area'] . ' </td>
+        //                 <td>' . $row['estado'] . ' </td>
+        //                 <td>' . $row['modelo'] . ' </td>
+        //                 <td>
+        //                     <button type="button" class="btn btn-square btn-primary m-1"><i class="fa fa-edit"></i></button>
+        //                     <button type="button" class="btn btn-square btn-warning m-1"><i class="fa fa-print"></i></button>
+        //                     <button type="button" class="btn btn-square btn-danger m-1"><i class="fa fa-trash"></i></button>
+        //                 </td>
+        //             </tr>
+        //         ';
+
+        //         echo $tablaComponentes;
+        //     }
+        //     break;
 }
--- modelo/mdl_Componentes.php
+++ modelo/mdl_Componentes.php
@@ -3,17 +3,10 @@ include_once("_CRUD.php");
 
 class Componentes extends CRUD
 {
-    function mostrarComponentes()
+    function mostrarUDN()
     {
-        $query = "SELECT *
-        FROM rfwsmqex_gvsl_sys_almacen.area_udn AS syscomareaudn
-        INNER JOIN rfwsmqex_gvsl_sys_almacen.areas AS areas 
-        ON syscomareaudn.id_Area = areas.idArea
-        INNER JOIN  rfwsmqex_gvsl_sys_almacen.componentes AS comp
-        ON syscomareaudn.idAreaUDN = comp.id_AreaUDN
-        INNER JOIN rfwsmqex_gvsl_sys_almacen.caracteristicas AS caract
-        ON comp.idComponente = caract.idCaracteristica AND caract.estado = 1";
-        $sql = $this->_Select($query, null, "2");
+        $query = "SELECT * FROM udn WHERE Stado = 1";
+        $sql = $this->_Select($query, null, "1");
         return $sql;
     }
 }
