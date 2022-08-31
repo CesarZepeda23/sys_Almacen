486916b1d04c2ba48f901bfeae832ff5754d47f8
TaniaGisell <88916403+TaniaGisell@users.noreply.github.com>
2022-08-31 00:39:15 -0500

    a

--- recursos/js/dashboard.js
+++ recursos/js/dashboard.js
@@ -1,3 +1,7 @@
+$("#btnVerUDN").click(function () {
+  $("#modalVerUDN").modal("show");
+});
+
 $(function mostrarTodosUDN() {
     let datos = new FormData();
     datos.append("opc", 1);
@@ -11,39 +15,5 @@ $(function mostrarTodosUDN() {
       success: function (respuesta) {
         $("#tablasUDNindex").html(respuesta);
      },
-    });
-  });
-  
-  $(function mostrarTodosEquipos() {
-    let datos = new FormData();
-    datos.append("opc", 2);
-    $.ajax({
-      type: "POST",
-      url: "./controlador/ctrl_Dashboard.php",
-      contentType: false,
-      data: datos,
-      processData: false,
-      cache: false,
-      success: function (respuesta) {
-        $("#tablasEquiposindex").html(respuesta);
-/*         console.log("Hola");
- */      },
-    });
-  });
-
-  $(function mostrarTodosComponentes() {
-    let datos = new FormData();
-    datos.append("opc", 3);
-      $.ajax({
-      type: "POST",
-      url: "./controlador/ctrl_Dashboard.php",
-      contentType: false,
-      data: datos,
-      processData: false,
-      cache: false,
-      success: function (respuesta) {
-        $("#tablascomponentesindex").html(respuesta);
-        /* console.log("Holaa") */
-      },
     });
   });
